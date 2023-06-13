1299cf12ee4f57fc326cc0db13f24d4837edb508
renusreeja <93851701+renusreeja@users.noreply.github.com>
2023-06-13 22:24:04 +0530

    updated

--- usercard3.js
+++ usercard3.js
@@ -10,9 +10,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data => {
-        displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
-        displayobject.imgurl = data.results[0].picture.large
-        displayobject.description = data.results[0].gender
+        //displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
+        displayobject.name = "renu" 
+        //displayobject.imgurl = data.results[0].picture.large
+        //displayobject.description = data.results[0].gender
         document.getElementById("mahesh-img").src = displayobject.imgurl;
         document.getElementById("maheshbabu").innerHTML = displayobject.name;
         document.getElementById("mahesh-desc").innerHTML = displayobject.description;
