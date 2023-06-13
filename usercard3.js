72c664fdc70c334f4996851c471bc5191fa300cd
renusreeja <93851701+renusreeja@users.noreply.github.com>
2023-06-13 22:19:27 +0530

    modified user card 3

--- usercard3.js
+++ usercard3.js
@@ -9,8 +9,13 @@ let displayobject;
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(data => console.log(data))
-    document.getElementById("mahesh-img").src = displayobject.imgurl;
-    document.getElementById("maheshbabu").innerHTML = displayobject.name;
-    document.getElementById("mahesh-desc").innerHTML = displayobject.description;
+    .then(data => {
+        displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
+        displayobject.imgurl = data.results[0].picture.large
+        displayobject.description = data.results[0].gender
+        document.getElementById("mahesh-img").src = displayobject.imgurl;
+        document.getElementById("maheshbabu").innerHTML = displayobject.name;
+        document.getElementById("mahesh-desc").innerHTML = displayobject.description;
+    })
+
 }
